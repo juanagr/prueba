e344a26f42c63e6e20b67d1c6895f1a6e6424aec
Juan Alejandro Garcia Rodriguez <JuanAGR@juans-mac.local>
2016-02-27 13:14:13 -0600

    Initial Commit

---
+++ pruebagithub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pruebagithub
+//
+//  Created by Juan Alejandro Garcia Rodriguez on 2/27/16.
+//  Copyright © 2016 Juan Alejandro Garcia Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ pruebagithub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  pruebagithub
+//
+//  Created by Juan Alejandro Garcia Rodriguez on 2/27/16.
+//  Copyright © 2016 Juan Alejandro Garcia Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
